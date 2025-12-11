2207a713277b10061be1f44b957526cf6a023ff5
Dee J. Mann <dee.j.mann@hotmail.com>
2025-12-10 23:54:09 -0500

    Added CORS handling for frontend Changes to be committed: 	modified:   BooksAPI/BooksAPI.http 	modified:   BooksAPI/Program.cs 	modified:   BooksAPI/Properties/launchSettings.json

--- backend/BooksAPI/Program.cs
+++ backend/BooksAPI/Program.cs
@@ -12,6 +12,16 @@ string? connString = config.GetConnectionString("DefaultConnection");
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontendDev", policy =>
+    {
+        policy.WithOrigins("http://localhost:5173", "https://localhost:5173")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
 builder.Services.AddOpenApi();
 
 WebApplication app = builder.Build();
@@ -23,6 +33,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("AllowFrontendDev");
 
 app.MapUsersEndpoints(connString);
 app.MapBooksEndpoints(connString);
